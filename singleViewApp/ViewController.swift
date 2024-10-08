1b360d5e71a1103a04ae96d1edbd8b65824c53e7
atabek-2004 <alialimzhanov2004@gmail.com>
2024-10-08 22:24:01 +0500

    first commit

---
+++ singleViewApp/ViewController.swift
@@ -0,0 +1,81 @@
+import UIKit
+
+class Contact {
+    var name: String
+    var phoneNumber: String
+    
+    init(name: String, phoneNumber: String) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+    }
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView: UITableView!
+    var contacts = [Contact]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Contacts"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addContact))
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        tableView = UITableView(frame: self.view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.addSubview(tableView)
+        
+        // Пример контактов
+        contacts.append(Contact(name: "John Doe", phoneNumber: "123-456-7890"))
+        contacts.append(Contact(name: "Jane Smith", phoneNumber: "098-765-4321"))
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let contact = contacts[indexPath.row]
+        cell.textLabel?.text = contact.name
+        cell.detailTextLabel?.text = contact.phoneNumber
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            contacts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @objc func addContact() {
+        let alert = UIAlertController(title: "New Contact", message: "Enter contact info", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Name"
+        }
+        alert.addTextField { textField in
+            textField.placeholder = "Phone Number"
+        }
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let nameField = alert.textFields?[0], let phoneField = alert.textFields?[1] else { return }
+            if let name = nameField.text, let phoneNumber = phoneField.text {
+                let newContact = Contact(name: name, phoneNumber: phoneNumber)
+                self.contacts.append(newContact)
+                self.tableView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
